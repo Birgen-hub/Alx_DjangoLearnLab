58d208ee5fe48cf45cf9db3d3fcfcf20a0902a89
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 13:42:22 +0300

    fix: Final update to query_samples.py using dedicated author_name variable

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,18 +10,18 @@ except Exception as e:
 from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries():
-    # Clean up existing data
     Author.objects.all().delete()
     
-    # --- 1. Create Sample Data ---
-    author1 = Author.objects.create(name='J.K. Rowling')
+    author_name = 'J.K. Rowling'
+    library_name = 'Central City Library'
+    
+    author1 = Author.objects.create(name=author_name)
     author2 = Author.objects.create(name='George Orwell')
     
     book1 = Book.objects.create(title='Harry Potter 1', author=author1)
     book2 = Book.objects.create(title='Harry Potter 2', author=author1)
     book3 = Book.objects.create(title='1984', author=author2)
     
-    library_name = 'Central City Library'
     library = Library.objects.create(name=library_name)
     library.books.add(book1, book3)
     
@@ -29,15 +29,13 @@ def run_queries():
     
     print("--- Sample Data Created ---")
     
-    # --- 2. Query all books by a specific author (ForeignKey) ---
     print("
 --- Query 1: Books by J.K. Rowling (ForeignKey) ---")
-    author = Author.objects.get(name='J.K. Rowling')
+    author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
     for book in books_by_author:
         print(f"- {book.title}")
         
-    # --- 3. List all books in a library (ManyToManyField) ---
     print("
 --- Query 2: Books in Central City Library (ManyToManyField) ---")
     library_obj = Library.objects.get(name=library_name)
@@ -45,7 +43,6 @@ def run_queries():
     for book in library_books:
         print(f"- {book.title} (by {book.author.name})")
         
-    # --- 4. Retrieve the librarian for a library (OneToOneField) ---
     print("
 --- Query 3: Librarian for Central City Library (OneToOneField) ---")
     library_obj = Library.objects.get(name=library_name)
