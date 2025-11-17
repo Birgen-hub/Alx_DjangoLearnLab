5a493eac254b949063d91bea4fcefc2e243bfcb1
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 14:53:24 +0300

    fix: Injected Book model and new permissions into bookshelf/models.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -21,3 +21,25 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField(default=2000)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_view_book", "Can view the list of books"),
+            ("can_create_book", "Can create a new book entry"),
+            ("can_edit_book", "Can edit an existing book entry"),
+            ("can_delete_book", "Can delete a book entry"),
+        ]
+
