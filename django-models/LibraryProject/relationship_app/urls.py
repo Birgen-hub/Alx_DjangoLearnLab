319a14de28e03cd9099960243ce48fcd0a6d6466
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 14:05:21 +0300

    feat: Implemented user authentication views, forms, URLs, and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LogoutView
+from .views import list_books, LibraryDetailView, CustomLoginView, register
 
 urlpatterns = [
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
+from .forms import CustomUserCreationForm
 
 def list_books(request):
     books = Book.objects.all().select_related('author')
@@ -13,3 +17,18 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     queryset = Library.objects.all().prefetch_related('books__author')
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
