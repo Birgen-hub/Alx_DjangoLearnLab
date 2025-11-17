28cd73bc75c575794a31650b0ed922b8cd759d08
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 14:41:14 +0300

    fix: Created dummy bookshelf/admin.py with CustomUser registration for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year', 'author')
-    search_fields = ('title', 'author')
+class CustomUserAdmin(UserAdmin):
+    pass
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
