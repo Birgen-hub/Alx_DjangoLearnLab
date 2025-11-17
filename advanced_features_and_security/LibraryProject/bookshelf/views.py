0ece5092464baf40e85280a5f00dd3ceab1911f2
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 15:04:34 +0300

    fix: Added required 'from .forms import ExampleForm' to bookshelf/views.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,27 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view_book', raise_exception=True)
 def book_list(request):
     search_term = request.GET.get('q', '')
     
-    # Secure Data Access: Using Django ORM filter() prevents SQL injection.
     if search_term:
         books = Book.objects.filter(title__icontains=search_term).all()
     else:
         books = Book.objects.all()
         
-    # Sanitize Output in Template (Implicitly handled by Django template engine)
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_edit_book', raise_exception=True)
 def form_example(request):
     if request.method == 'POST':
-        return HttpResponse('Form submitted securely')
-    return render(request, 'bookshelf/form_example.html')
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('Form submitted securely and valid')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
-# Include dummy views required by the previous step to avoid breaking existing checks
 @permission_required('bookshelf.can_edit_book', raise_exception=True)
 def book_edit(request, pk):
     return HttpResponse('Book edit secure')
