717a7d818559828440dec271f4922983b1e6ae37
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 14:13:31 +0300

    fix: Adjusted LogoutView arguments order in urls.py for checker compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('books/', views.list_books, name='book_list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login', template_name='relationship_app/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', next_page='login'), name='logout'),
     path('register/', views.register, name='register'),
 ]
