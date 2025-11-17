5e25b26e166ea4f109fe5f312d5b3f588ae9a115
Dolly Birgen <dollybirgen@gmail.com>
2025-11-17 13:46:48 +0300

    feat: Added function and class based views, URLs, and templates to relationship_app

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+def book_list(request):
+    books = Book.objects.all().select_related('author')
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    queryset = Library.objects.all().prefetch_related('books__author')
