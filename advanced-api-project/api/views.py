9f3f2b0058807921fd1059048e5f945b77e931fb
Dolly Birgen <dollybirgen@gmail.com>
2025-11-27 22:29:05 +0300

    feat: Applied IsAuthenticatedOrReadOnly permissions to all API views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -6,18 +7,26 @@ from .serializers import AuthorSerializer, BookSerializer
 class AuthorListCreate(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    # Applying permissions
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Maps to Retrieve, Update, and Destroy (equivalent to DetailView, UpdateView, DeleteView functionality)
 class AuthorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    # Applying permissions
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Maps to List and Create
 class BookListCreate(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # Applying permissions
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Maps to Retrieve, Update, and Destroy
 class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # Applying permissions
+    permission_classes = [IsAuthenticatedOrReadOnly]
