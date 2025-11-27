852fa0ee4e972954b40b0983bdd2e7552e7f6010
Dolly Birgen <dollybirgen@gmail.com>
2025-11-27 22:18:43 +0300

    feat: Implemented BookList (ListAPIView) and BookViewSet (CRUD operations with Router) for api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,11 @@
-from rest_framework import generics, viewsets, permissions
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import BookViewSet
-from rest_framework.authtoken import views as drf_views
-
-router = DefaultRouter()
-router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/api-token-auth/', drf_views.obtain_auth_token),
+    path('api/', include('api.urls')),
 ]
