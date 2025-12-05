05c4196b2df8888ca9c39850ea80e998f517002d
Dolly Birgen <dollybirgen@gmail.com>
2025-12-05 13:07:57 +0300

    Fix: Model dependency and final attempt on TagWidget form error

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,12 @@
 from django.db import models
-from django.urls import reverse
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("posts_by_tag", args=[self.name])
-
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+    
+    # This must exist for PostForm to work with tags
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("post_detail", args=[str(self.id)])
