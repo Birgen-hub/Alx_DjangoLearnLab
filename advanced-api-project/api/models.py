918e77ab3e13bfd49ae4bf1008ce69be967ec2d2
Dolly Birgen <dollybirgen@gmail.com>
2025-12-05 11:30:13 +0300

    feat: Implemented Author/Book models, nested serializers, and publication year validation.

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
