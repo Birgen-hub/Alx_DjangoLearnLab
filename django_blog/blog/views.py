577f5b524b9524418e569e3274f796bae99ff3af
Dolly Birgen <dollybirgen@gmail.com>
2025-12-05 11:39:00 +0300

    feat: Initial blog setup, Post model, and full authentication system (register, login, profile).

---
+++ django_blog/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.contrib import messages
+from .forms import UserRegisterForm, ProfileUpdateForm
+from .models import Post
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Account created for {user.username}!')
+            return redirect('post_list')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+post_list = PostListView.as_view()
