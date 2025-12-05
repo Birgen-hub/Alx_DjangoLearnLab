3783e9e7d794372a4bbf46d8a54d8e87eeac6cad
Dolly Birgen <dollybirgen@gmail.com>
2025-12-05 13:09:33 +0300

    Fix: Comprehensive model, form, and migration update for TagWidget

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     
-    # This must exist for PostForm to work with tags
     tags = TaggableManager()
 
     def __str__(self):
