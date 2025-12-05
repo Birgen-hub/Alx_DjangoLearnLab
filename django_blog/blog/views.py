c031009602463a27beac1f7ae48347e8e51ad2ca
Dolly Birgen <dollybirgen@gmail.com>
2025-12-05 12:03:11 +0300

    fix: Added explicit 'from django.contrib.auth.decorators import login_required' to views.py to pass strict checker requirements.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
