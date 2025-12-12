2cb5118d10b0b4b4ca3a633dbbd8c433a7f74bad
Dolly Birgen <dollybirgen@gmail.com>
2025-12-12 20:38:12 +0300

    Removed recipe_api project completely

--- recipe_api/README.md
+++
@@ -1,7 +0,0 @@
-# Recipe Management API
-
-## Setup
-1. Clone repository.
-2. Activate virtual environment (source venv/Scripts/activate).
-3. Run migrations and create superuser: 'python manage.py migrate' & 'python manage.py createsuperuser'.
-4. Run server: 'python manage.py runserver'.
--- recipe_api/recipe_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from knox import views as knox_views
-from django.conf.urls.static import static
-
-    path('api/v1/auth/logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
-    
-]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
