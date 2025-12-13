023118c14a4caaaf2c95be6cffdb1caf8f92242e
Dolly Birgen <dollybirgen@gmail.com>
2025-12-13 23:01:30 +0300

    Add following field to CustomUser model for follow functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,21 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(
-        upload_to='profile_pictures/',
-        blank=True,
-        null=True
-    )
-    followers = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='following',
-        blank=True
-    )
-
-    def __str__(self):
-        return self.username
-
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
