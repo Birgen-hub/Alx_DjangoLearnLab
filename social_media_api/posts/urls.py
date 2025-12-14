1e3c190cddb8e135f805da17d3e3d79e5fcdf5d9
Dolly Birgen <dollybirgen@gmail.com>
2025-12-14 21:54:28 +0300

    Implement Posts and Comments functionality with serializers, viewsets, and routes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,11 @@
-from rest_framework import routers
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
