da8360120a9f99a065cd74090ba16cb715bf09f3
Dolly Birgen <dollybirgen@gmail.com>
2025-12-15 13:11:31 +0300

    fix(urls): Ensured registration, login, and profile URLs are correctly defined.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from .views import LoginView
+from .views import (
+    RegisterUserView,
+    LoginUserView,
+    ManageUserView,
+    FollowUserView,
+    UnfollowUserView
+)
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('me/', ManageUserView.as_view(), name='me'),
+    
+    # Social Interaction URLs
+    path('<int:pk>/follow/', FollowUserView.as_view(), name='follow-user'),
+    path('<int:pk>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
