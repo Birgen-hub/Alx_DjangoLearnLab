a2635e7f8fda64c072738de24916ce1d2235b9b3
Dolly Birgen <dollybirgen@gmail.com>
2025-12-15 08:53:33 +0300

    Add feed view based on followed users posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,19 +1,35 @@
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
+
+class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # users the current user follows
+        following_users = user.following.all()
+
+        # posts from followed users ordered by most recent
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
+
