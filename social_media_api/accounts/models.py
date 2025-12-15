3d3f3074ad6b03ccfafc9852a79a545b34c220d8
Dolly Birgen <dollybirgen@gmail.com>
2025-12-15 09:12:35 +0300

    Add profile_picture ImageField to custom user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,10 +1,15 @@
-from django.db import models
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.URLField(blank=True, null=True)
-    
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
-    def __str__(self): return self.username
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True
+    )
+
